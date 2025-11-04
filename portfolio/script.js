b75a0a163198719b94043dc4d93b018877c04e56
Tjay05 <tjayolatayo@gmail.com>
2025-11-04 13:45:54 +0100

    Add function to hide nav when a link is clicked

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,12 +1,18 @@
 const hBurger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
 const offCanvas = document.getElementById('offside-canvas');
 
 hBurger.addEventListener('click', function () {
-  if (offCanvas.className === 'hidden') {
+  if (offCanvas.className === '') {
     offCanvas.className = 'offside-canvas';
     hBurger.classList.add('open');
+    
+    menu.addEventListener('click', () => {
+      hBurger.classList.remove('open');
+      offCanvas.classList.remove('offside-canvas');
+    })
   } else {
-    offCanvas.className = 'hidden';
     hBurger.classList.remove('open');
+    offCanvas.classList.remove('offside-canvas');
   }
-})
+});
