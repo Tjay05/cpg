3279e0d73cc862487bd5138d5e9552c93edb28ec
Tjay05 <tjayolatayo@gmail.com>
2025-11-15 09:36:49 +0100

    Add sub,it logic

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,6 +2,8 @@ const hBurger = document.getElementById('hamburger');
 const menu = document.getElementById('menu');
 const dropNav = document.getElementById('drop-nav');
 
+const contactForm = document.getElementById('contact-form');
+
 hBurger.addEventListener('click', function () {
   if (dropNav.className === '') {
     dropNav.className = 'drop-nav';
@@ -16,3 +18,29 @@ hBurger.addEventListener('click', function () {
     dropNav.classList.remove('drop-nav');
   }
 });
+
+const handleSubmit = async (url, username, email, message) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ name: username, email, message })
+  })
+  const data = await response.json();
+  if(!response.ok) {
+    console.log('Error while fetching');
+  }
+  if (response.ok) {
+    console.log(data);
+  }
+}
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  handleSubmit('http://localhost:4000/form', username, email, message);
+
+})
